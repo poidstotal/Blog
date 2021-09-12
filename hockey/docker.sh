337e674b06017f36065bd4205b838f8271a03cce
Gilbert M <gilbertm@GilbertM-Personal.local>
2021-09-12 16:47:18 +0800

    hockey game

--- hockey/docker.sh
+++ hockey/docker.sh
@@ -1,6 +1,6 @@
 docker run -d -it \
     -v "$PWD":/var/www/html/:rw \
-    --name phpServer --hostname phpServer \
+    --name hockeyApp --hostname hockeyApp \
     -w /var/www/html \
     -p 80:80 \
     php:7.2-apache
