87bb69b8fa6a3e917c71df2ff413e492b4e84328
Gilbert M <gilbertm@GilbertM-Personal.local>
2021-09-12 16:21:08 +0800

    hockey game

--- hockey/index.php
+++ hockey/index.php
@@ -78,56 +78,11 @@
     # show the table
     echo '<table border=0 colspan=0>'.$htmldata.'</table>';
 
-
+/*
     #datastructure
     echo "<pre>";
     print_r($finalRes) ;
     echo "</pre>";
-
-    /*
-    $finalRes=
-    $leagueRes=array_merge($est,$west,$finalRes);
-
-    # run all games for all rounds within a division
-    function getDivRes($name){
-        #first round
-        # intial 8 teams are labeled A to H
-        $teams= range("A","H");
-        $round1Res=getRoundRes($teams,0,$name);
-
-        # Second round
-        # team are winners from 1st round
-        $teams= $round1Res['winners'];
-        $round2Res=getRoundRes($teams,1,$name);
-
-        # Third round
-        # team are winners from 2nd round
-        $teams= $round2Res['winners'];
-        $round3Res=getRoundRes($teams,2,$name);
-
-        # Combine all result from rounds
-        $divRes = array($round1Res['Games'],$round2Res['Games'], $round3Res['Games']);
-        $divRes = array_merge(...$divRes);
-        return $divRes;
-    }
-    # Final game
-    $est=getDivRes("East");
-    $west=getDivRes("West");
-    $teams=[$est[6]['winner'],$west[6]['winner']];
-    $finalRes=getRoundRes($teams,"Final","Final")['Games'];
-    $leagueRes=array_merge($est,$west,$finalRes);
-
-    # Display result as html table
-    $htmldata='<table border=0 colspan="0"><tr><th>div</th><th>round</th><th> home</th><th> away</th><th>winner</th><th>wins</th><th>loss</th></tr>';
-
-    foreach($leagueRes as $data){
-        $htmldata.='<tr><td>'.$data["div"].'</td><td>'.$data["round"].'</td><td>'.$data["home"].'</td><td>'.$data["away"].'</td><td>'.$data["winner"].'</td><td>'.$data["wins"].'</td><td>'.$data['loss'].'</td></tr>';
-    }
-    echo $htmldata.'</table>';
-
-
-
-
 */
 
 
